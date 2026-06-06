c51743967b82c8d48b09720cdb75e2215eba83fb
Enorian <enorian.rajo@gmail.com>
2026-06-06 13:13:57 +0200

    Fix entities relation

--- src/core/external/entities/student.entity.ts
+++ src/core/external/entities/student.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, OneToMany, OneToOne } from 'typeorm';
+import { Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 import { InternalStudent } from '@dad-group-1/backend-common';
 import { User } from './user.entity';
 import { Enrollment } from '../../enrollments/entities/enrollment.entity';
@@ -6,6 +6,7 @@ import { Enrollment } from '../../enrollments/entities/enrollment.entity';
 @Entity()
 export class Student extends InternalStudent {
   @OneToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'user_id' })
   user: User;
   @OneToMany(() => Enrollment, (enrollment) => enrollment.student)
   enrollments: Enrollment[];
